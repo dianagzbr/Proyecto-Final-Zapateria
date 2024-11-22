ba10bf528a8d33926ccbbf620b9c41eb8af05fc4
dianagzbr <alejabribirk4@gmail.com>
2024-11-22 15:12:52 -0600

    Seeders de Comprobante y usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(DocumentoSeeder::class);
+        //$this->call(DocumentoSeeder::class);
+        $this->call(ComprobanteSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
